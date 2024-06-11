982dd98efca9ade5c80ceb22737831d8578a851d
Akash Kadlag <akashkadlag14@gmail.com>
2024-06-11 17:34:32 +0530

    feat: build the navbar

---
+++ src/app/NavBar.tsx
@@ -0,0 +1,57 @@
+'use client';
+
+import {Container, Flex} from '@radix-ui/themes';
+import classNames from 'classnames';
+import Link from 'next/link';
+import {usePathname} from 'next/navigation';
+import {AiFillBug} from 'react-icons/ai';
+
+const NavBar = () => {
+	return (
+		<nav className="border-b mb-5 px-5 py-3 dark:border-zinc-800">
+			<Container>
+				<Flex justify="between">
+					<Flex align="center" gap="5">
+						<Link href="/" className="mr-1">
+							<AiFillBug size="18" />
+						</Link>
+						<Navlinks />
+					</Flex>
+					<Flex align="center" gap="5">
+						<span className="nav-link">Theme</span>
+						<span className="nav-link">Login</span>
+					</Flex>
+				</Flex>
+			</Container>
+		</nav>
+	);
+};
+
+const Navlinks = () => {
+	const currentPath = usePathname();
+
+	const links = [
+		{label: 'Dashboard', href: '/dashboard'},
+		{label: 'Issues', href: '/issues'},
+	];
+
+	return (
+		<ul className="flex space-x-6">
+			{links.map((link, index) => (
+				<li key={index}>
+					<Link
+						href={link.href}
+						className={classNames({
+							'nav-link': true,
+							'!text-zinc-900 dark:!text-zinc-50': link.href === currentPath,
+						})}
+					>
+						{link.label}
+					</Link>
+				</li>
+			))}
+		</ul>
+	);
+};
+
+export default NavBar;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
-import {Theme} from '@radix-ui/themes';
+import {Container, Theme} from '@radix-ui/themes';
 import type {Metadata} from 'next';
 import {Inter} from 'next/font/google';
 import './globals.css';
+import NavBar from './NavBar';
 
 const inter = Inter({
 	subsets: ['latin'],
@@ -17,7 +18,12 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<body className={inter.variable}>
-				<Theme>{children}</Theme>
+				<Theme>
+					<NavBar />
+					<main className="p-5">
+						<Container>{children}</Container>
+					</main>
+				</Theme>
 			</body>
 		</html>
 	);
