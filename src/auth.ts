a0e52f5aed65ed033b8e8a152d51922a085d5fa8
Akash Kadlag <akashkadlag14@gmail.com>
2024-06-13 17:06:45 +0530

    feat: add the prisma adaptor

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,10 @@
 import authConfig from '@/auth.config';
+import prisma from '@/prisma/client';
+import {PrismaAdapter} from '@auth/prisma-adapter';
 import NextAuth from 'next-auth';
 
 export const {handlers, auth, signIn, signOut} = NextAuth({
+  adapter: PrismaAdapter(prisma),
   session: {strategy: 'jwt'},
   ...authConfig,
 });
