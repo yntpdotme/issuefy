8756c7a7db52974f93e94e126b6c7b770a2a33cf
Akash Kadlag <akashkadlag14@gmail.com>
2024-06-19 17:31:59 +0530

    feat: implement custom hook to get current user

---
+++ src/hooks/useCurrentUser.ts
@@ -0,0 +1,8 @@
+import {useSession} from 'next-auth/react';
+
+export const useCurrentUser = () => {
+  const session = useSession();
+
+  return {status: session.status, user: session.data?.user};
+};
+ 
