671d1748d0dd78987c46e22420bbe8eaec6ccce7
Akash Kadlag <akashkadlag14@gmail.com>
2024-06-26 14:43:39 +0530

    style: add loading skeleton for issues table

---
+++ src/app/issues/loading.tsx
@@ -0,0 +1,48 @@
+import Skeleton from '@/components/Skeleton';
+import {Button, Flex, Table} from '@radix-ui/themes';
+import Link from 'next/link';
+
+const LoadingIssuesPage = () => {
+  const issues = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  return (
+    <Flex direction="column" gap="18px">
+      <Button className="w-fit">
+        <Link href="/issues/new">New Issue</Link>
+      </Button>
+      <Table.Root variant="surface">
+        <Table.Header>
+          <Table.Row>
+            <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Status
+            </Table.ColumnHeaderCell>
+            <Table.ColumnHeaderCell className="hidden md:table-cell">
+              Created
+            </Table.ColumnHeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {issues.map(issue => (
+            <Table.Row key={issue}>
+              <Table.Cell>
+                <Skeleton />
+                <div className="block md:hidden">
+                  <Skeleton />
+                </div>
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                <Skeleton />
+              </Table.Cell>
+              <Table.Cell className="hidden md:table-cell">
+                <Skeleton />
+              </Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table.Root>
+    </Flex>
+  );
+};
+
+export default LoadingIssuesPage;
