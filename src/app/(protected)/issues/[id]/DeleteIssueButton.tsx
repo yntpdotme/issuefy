f73247aa3e58a1ddd41489d0ed6933d0683c84a2
Akash Kadlag <akashkadlag14@gmail.com>
2024-06-30 18:08:46 +0530

    feat: show a confirmation dialog box

--- src/app/(protected)/issues/[id]/DeleteIssueButton.tsx
+++ src/app/(protected)/issues/[id]/DeleteIssueButton.tsx
@@ -1,10 +1,10 @@
 'use client';
-import {Button} from '@radix-ui/themes';
-import {useRouter} from 'next/navigation';
+import {Spinner} from '@/components/Spinner';
+import {AlertDialog, Button, Flex} from '@radix-ui/themes';
 import {useState} from 'react';
+import {MdDelete} from 'react-icons/md';
 
 const DeleteIssueButton = ({issueId}: {issueId: number}) => {
-  const router = useRouter();
   const [error, setError] = useState(false);
   const [isDeleting, setDeleting] = useState(false);
 
@@ -13,9 +13,57 @@ const DeleteIssueButton = ({issueId}: {issueId: number}) => {
   };
 
   return (
-    <Button color="red" onClick={deleteIssue}>
-      Delete Issue
-    </Button>
+    <>
+      <AlertDialog.Root>
+        <AlertDialog.Trigger>
+          <Button color="red" disabled={isDeleting}>
+            <MdDelete /> Delete Issue
+            {isDeleting && <Spinner />}
+          </Button>
+        </AlertDialog.Trigger>
+
+        <AlertDialog.Content maxWidth="450px">
+          <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+
+          <AlertDialog.Description size="2">
+            Are you sure you want ot delete this issue? This action cannot be
+            undone.
+          </AlertDialog.Description>
+
+          <Flex gap="3" mt="4">
+            <AlertDialog.Cancel>
+              <Button variant="soft" color="gray">
+                Cancel
+              </Button>
+            </AlertDialog.Cancel>
+            <AlertDialog.Action>
+              <Button variant="solid" color="red" onClick={deleteIssue}>
+                Delete Issue
+              </Button>
+            </AlertDialog.Action>
+          </Flex>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+
+      <AlertDialog.Root open={error}>
+        <AlertDialog.Content maxWidth="450px">
+          <AlertDialog.Title>Error</AlertDialog.Title>
+
+          <AlertDialog.Description size="2">
+            This issue could not be deleted.
+          </AlertDialog.Description>
+
+          <Button
+            color="gray"
+            variant="soft"
+            mt="4"
+            onClick={() => setError(false)}
+          >
+            OK
+          </Button>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+    </>
   );
 };
 
