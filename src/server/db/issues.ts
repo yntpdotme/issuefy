1729c13cc8438875f315bd33e229d9f66a988d4c
Akash Kadlag <akashkadlag14@gmail.com>
2024-07-17 15:28:07 +0530

    feat: optimize performance using react cache

--- src/server/db/issues.ts
+++ src/server/db/issues.ts
@@ -1,5 +1,6 @@
 import prisma from '@/prisma/client';
 import {Issue, Prisma, Status} from '@prisma/client';
+import {cache} from 'react';
 
 export const createIssue = async (issue: Prisma.IssueCreateInput) => {
   const newIssue = await prisma.issue.create({data: issue});
@@ -7,44 +8,46 @@ export const createIssue = async (issue: Prisma.IssueCreateInput) => {
   return newIssue;
 };
 
-export const getIssues = async (
-  params: {
-    status?: Status;
-    orderBy?: keyof Pick<Issue, 'title' | 'status' | 'createdAt'>;
-    page?: number;
-    pageSize?: number;
-  } = {},
-) => {
-  const {status, orderBy, page = 1, pageSize = 10} = params;
+export const getIssues = cache(
+  async (
+    params: {
+      status?: Status;
+      orderBy?: keyof Pick<Issue, 'title' | 'status' | 'createdAt'>;
+      page?: number;
+      pageSize?: number;
+    } = {},
+  ) => {
+    const {status, orderBy, page = 1, pageSize = 10} = params;
 
-  const issues = await prisma.issue.findMany({
-    where: {status},
-    orderBy: orderBy
-      ? [{[orderBy]: 'asc'}, {createdAt: 'desc'}]
-      : {createdAt: 'desc'},
-    skip: (page - 1) * pageSize,
-    take: pageSize,
-  });
+    const issues = await prisma.issue.findMany({
+      where: {status},
+      orderBy: orderBy
+        ? [{[orderBy]: 'asc'}, {createdAt: 'desc'}]
+        : {createdAt: 'desc'},
+      skip: (page - 1) * pageSize,
+      take: pageSize,
+    });
 
-  const totalIssues = await prisma.issue.count({
-    where: {status},
-  });
+    const totalIssues = await prisma.issue.count({
+      where: {status},
+    });
 
-  return {
-    data: issues,
-    currentPage: page,
-    pageSize,
-    totalIssues,
-  };
-};
+    return {
+      data: issues,
+      currentPage: page,
+      pageSize,
+      totalIssues,
+    };
+  },
+);
 
-export const getIssue = async (id: number) => {
+export const getIssue = cache(async (id: number) => {
   const issue = await prisma.issue.findUnique({
     where: {id},
   });
 
   return issue;
-};
+});
 
 export const updateIssue = async (
   id: number,
@@ -62,37 +65,45 @@ export const deleteIssue = async (id: number) => {
   await prisma.issue.delete({where: {id}});
 };
 
-export const getIssuesWithUsers = async (
-  params: {
-    status?: Status;
-    orderBy?: keyof Pick<Issue, 'title' | 'status' | 'createdAt'>;
-    page?: number;
-    pageSize?: number;
-    includeUser?: boolean;
-  } = {},
-) => {
-  const {status, orderBy, page = 1, pageSize = 10, includeUser = true} = params;
+export const getIssuesWithUsers = cache(
+  async (
+    params: {
+      status?: Status;
+      orderBy?: keyof Pick<Issue, 'title' | 'status' | 'createdAt'>;
+      page?: number;
+      pageSize?: number;
+      includeUser?: boolean;
+    } = {},
+  ) => {
+    const {
+      status,
+      orderBy,
+      page = 1,
+      pageSize = 10,
+      includeUser = true,
+    } = params;
 
-  const issues = await prisma.issue.findMany({
-    where: {status},
-    orderBy: orderBy
-      ? [{[orderBy]: 'asc'}, {createdAt: 'desc'}]
-      : {createdAt: 'desc'},
-    skip: (page - 1) * pageSize,
-    take: pageSize,
-    include: {
-      assignedToUser: includeUser,
-    },
-  });
+    const issues = await prisma.issue.findMany({
+      where: {status},
+      orderBy: orderBy
+        ? [{[orderBy]: 'asc'}, {createdAt: 'desc'}]
+        : {createdAt: 'desc'},
+      skip: (page - 1) * pageSize,
+      take: pageSize,
+      include: {
+        assignedToUser: includeUser,
+      },
+    });
 
-  const totalIssues = await prisma.issue.count({
-    where: {status},
-  });
+    const totalIssues = await prisma.issue.count({
+      where: {status},
+    });
 
-  return {
-    data: issues,
-    currentPage: page,
-    pageSize,
-    totalIssues,
-  };
-};
+    return {
+      data: issues,
+      currentPage: page,
+      pageSize,
+      totalIssues,
+    };
+  },
+);
